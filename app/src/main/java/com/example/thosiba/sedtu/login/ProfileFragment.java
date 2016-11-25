60915e74488896bbc95e0d51efad7dfa506c3f1c
luis_e_ramirez <luis_e_3194@hotmail.com>
2016-11-25 10:15:19 -0600

    a ver...

--- app/src/main/java/com/example/thosiba/sedtu/login/ProfileFragment.java
+++ app/src/main/java/com/example/thosiba/sedtu/login/ProfileFragment.java
@@ -1,11 +1,193 @@
 package com.example.thosiba.sedtu.login;
 
+import android.content.DialogInterface;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatButton;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.thosiba.sedtu.R;
+import com.example.thosiba.sedtu.modelos.ServerRequest;
+import com.example.thosiba.sedtu.modelos.ServerResponse;
+import com.example.thosiba.sedtu.modelos.Usuario;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * Created by thosiba on 09/11/2016.
  */
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener{
+
+    private TextView tv_name,tv_email,tv_message;
+    private SharedPreferences pref;
+    private AppCompatButton btn_change_password,btn_logout;
+    private EditText et_old_password,et_new_password;
+    private AlertDialog dialog;
+    private ProgressBar progress;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        initViews(view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        pref = getActivity().getPreferences(0);
+        tv_name.setText("Welcome : "+pref.getString(Constantes.NAME,""));
+        tv_email.setText(pref.getString(Constantes.EMAIL,""));
+
+    }
+
+    private void initViews(View view){
+
+        tv_name = (TextView)view.findViewById(R.id.tv_name);
+        tv_email = (TextView)view.findViewById(R.id.tv_email);
+        btn_change_password = (AppCompatButton)view.findViewById(R.id.btn_chg_password);
+        btn_logout = (AppCompatButton)view.findViewById(R.id.btn_logout);
+        btn_change_password.setOnClickListener(this);
+        btn_logout.setOnClickListener(this);
+
+    }
+
+    private void showDialog(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_change_password, null);
+        et_old_password = (EditText)view.findViewById(R.id.et_old_password);
+        et_new_password = (EditText)view.findViewById(R.id.et_new_password);
+        tv_message = (TextView)view.findViewById(R.id.tv_message);
+        progress = (ProgressBar)view.findViewById(R.id.progress);
+        builder.setView(view);
+        builder.setTitle("Change Password");
+        builder.setPositiveButton("Change Password", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog = builder.create();
+        dialog.show();
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String old_password = et_old_password.getText().toString();
+                String new_password = et_new_password.getText().toString();
+                if(!old_password.isEmpty() && !new_password.isEmpty()){
+
+                    progress.setVisibility(View.VISIBLE);
+                    changePasswordProcess(pref.getString(Constantes.EMAIL,""),old_password,new_password);
+
+                }else {
+
+                    tv_message.setVisibility(View.VISIBLE);
+                    tv_message.setText("Fields are empty");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+
+            case R.id.btn_chg_password:
+                showDialog();
+                break;
+            case R.id.btn_logout:
+                logout();
+                break;
+        }
+    }
+
+    private void logout() {
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean(Constantes.IS_LOGGED_IN,false);
+        editor.putString(Constantes.EMAIL,"");
+        editor.putString(Constantes.NAME,"");
+        editor.apply();
+        goToLogin();
+    }
+
+    private void goToLogin(){
+
+        Fragment login = new LoginFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_frame,login);
+        ft.commit();
+    }
+
+    private void changePasswordProcess(String email,String old_password,String new_password){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constantes.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        RequestInterface requestInterface = retrofit.create(RequestInterface.class);
+
+        Usuario user = new Usuario();
+        user.setEmail(email);
+        user.setOld_password(old_password);
+        user.setNew_password(new_password);
+        ServerRequest request = new ServerRequest();
+        request.setOperation(Constantes.CHANGE_PASSWORD_OPERATION);
+        request.setUsuario(user);
+        Call<ServerResponse> response = requestInterface.operation(request);
+
+        response.enqueue(new Callback<ServerResponse>() {
+            @Override
+            public void onResponse(Call<ServerResponse> call, retrofit2.Response<ServerResponse> response) {
+
+                ServerResponse resp = response.body();
+                if(resp.getResult().equals(Constantes.SUCCESS)){
+                    progress.setVisibility(View.GONE);
+                    tv_message.setVisibility(View.GONE);
+                    dialog.dismiss();
+                    Snackbar.make(getView(), resp.getMessage(), Snackbar.LENGTH_LONG).show();
+
+                }else {
+                    progress.setVisibility(View.GONE);
+                    tv_message.setVisibility(View.VISIBLE);
+                    tv_message.setText(resp.getMessage());
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ServerResponse> call, Throwable t) {
+
+                Log.d(Constantes.TAG,"failed");
+                progress.setVisibility(View.GONE);
+                tv_message.setVisibility(View.VISIBLE);
+                tv_message.setText(t.getLocalizedMessage());
+
+            }
+        });
+    }
 
 }
